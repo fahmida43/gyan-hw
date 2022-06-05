5d69c22b3bab2a36fea0a99e5d8f7213a3f69ef0
fahmida43 <99779584+fahmida43@users.noreply.github.com>
2022-06-05 09:40:38 +0600

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,16 @@
+//banner slider js start
+$('.banner_slider').slick({
+    prevArrow:'<span class="priv_arrow"><i class="fa-solid fa-angle-left"></i></span>',
+    nextArrow:'<span class="next_arrow"><i class="fa-solid fa-angle-right"></i></span>',
+    autoplaySpeed:500,
+    dots:false,
+  });
+//banner slider js end//
+//About slider js start
+$('.about_slider').slick({
+    prevArrow:false,
+    nextArrow:false,
+    autoplay:true,
+    autoplaySpeed:900,
+});
+//About slider js end
